7299cde3fad8cb23326e3b59aeb1898f97128c69
shaaa234 <149248739+shaaa234@users.noreply.github.com>
2023-10-29 00:03:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,36 @@
+const toBool = (x) => x == 'true'
+const { Sequelize } = require('sequelize')
+const { existsSync } = require('fs')
+const path = require('path')
+const configPath = path.join(__dirname, './config.env')
+const databasePath = path.join(__dirname, './database.db')
+if (existsSync(configPath)) require('dotenv').config({ path: configPath })
+const DATABASE_URL =
+  process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
+module.exports = {
+  VERSION: require('./package.json').version,
+  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  DATABASE:
+    DATABASE_URL === databasePath
+      ? new Sequelize({
+          dialect: 'sqlite',
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: 'postgres',
+          ssl: true,
+          protocol: 'postgres',
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+  SUDO: process.env.SUDO || '',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+  BRANCH: 'master',
+  MENTION: process.env.MENTION || '',
+}

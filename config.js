d8581589dec981864229d26f29fddf1b6177c85c
shaaa234 <149248739+shaaa234@users.noreply.github.com>
2023-10-29 00:14:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,6 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
